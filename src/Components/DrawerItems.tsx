116b693b4d8d6662d7ccb2b3f2bb8749b9e40020
aprince <gaprincipec@unc.edu.pe>
2021-06-03 15:28:09 -0500

    .

--- src/Components/DrawerItems.tsx
+++ src/Components/DrawerItems.tsx
@@ -23,26 +23,26 @@ const DrawerItems: React.FC = () => {
     return (
         <>
             <List component='nav'>
-                <ListItem component={Link} to='/inicio' button selected={selectedIndex === 0} onClick={(event:any) => handleListItemClick(event, 0)}>
+                <ListItem component={Link} to='inicio' button selected={selectedIndex === 0} onClick={(event:any) => handleListItemClick(event, 0)}>
                     <ListItemIcon>
                         <Home />
                     </ListItemIcon>
                     <ListItemText primary='Inicio' />
                 </ListItem>
-                <ListItem component={Link} to='/incidencias' button selected={selectedIndex === 1} onClick={(event:any) => handleListItemClick(event, 1)}>
+                <ListItem component={Link} to='incidencias' button selected={selectedIndex === 1} onClick={(event:any) => handleListItemClick(event, 1)}>
                     <ListItemIcon>
                         <DoubleArrowIcon />
                     </ListItemIcon>
                     <ListItemText primary='Incidencias' />
                 </ListItem>
 
-                <ListItem component={Link} to='/transmisiones' button selected={selectedIndex === 2} onClick={(event:any) => handleListItemClick(event, 2)}>
+                <ListItem component={Link} to='transmisiones' button selected={selectedIndex === 2} onClick={(event:any) => handleListItemClick(event, 2)}>
                     <ListItemIcon>
                         <OndemandVideoIcon />
                     </ListItemIcon>
                     <ListItemText primary='Transmisiones' />
                 </ListItem>
-                <ListItem component={Link} to='/videos' button selected={selectedIndex === 3} onClick={(event:any) => handleListItemClick(event, 3)}>
+                <ListItem component={Link} to='videos' button selected={selectedIndex === 3} onClick={(event:any) => handleListItemClick(event, 3)}>
                     <ListItemIcon>
                         <VideoLibraryIcon />
                     </ListItemIcon>
@@ -58,37 +58,37 @@ const DrawerItems: React.FC = () => {
                 </ListItem>
                 <Collapse in={open} timeout="auto" unmountOnExit>
                     <List component="div" style={{marginLeft:15}} disablePadding>
-                    <ListItem button  component={Link} to='/usuario' selected={selectedIndex === 4} onClick={(event:any) => handleListItemClick(event, 4)}>
+                    <ListItem button  component={Link} to='usuario' selected={selectedIndex === 4} onClick={(event:any) => handleListItemClick(event, 4)}>
                             <ListItemIcon>
                                 <PeopleIcon />
                             </ListItemIcon>
                             <ListItemText primary="Usuario" />
                         </ListItem>
-                    <ListItem button  component={Link} to='/personal-campo' selected={selectedIndex === 5} onClick={(event:any) => handleListItemClick(event, 5)}>
+                    <ListItem button  component={Link} to='personal-campo' selected={selectedIndex === 5} onClick={(event:any) => handleListItemClick(event, 5)}>
                             <ListItemIcon>
                                 <PeopleIcon />
                             </ListItemIcon>
                             <ListItemText primary="Personal de campo" />
                         </ListItem>
-                        <ListItem button  component={Link} to='/categoria' selected={selectedIndex === 6} onClick={(event:any) => handleListItemClick(event, 6)}>
+                        <ListItem button  component={Link} to='categoria' selected={selectedIndex === 6} onClick={(event:any) => handleListItemClick(event, 6)}>
                             <ListItemIcon>
                                 <CategoryIcon />
                             </ListItemIcon>
                             <ListItemText primary="Categoria de incidencias" />
                         </ListItem>
-                        <ListItem button  component={Link} to='/tipo' selected={selectedIndex === 7} onClick={(event:any) => handleListItemClick(event, 7)}>
+                        <ListItem button  component={Link} to='tipo' selected={selectedIndex === 7} onClick={(event:any) => handleListItemClick(event, 7)}>
                             <ListItemIcon>
                                 <LayersIcon />
                             </ListItemIcon>
                             <ListItemText primary="Tipo de incidencia" />
                         </ListItem>
-                        <ListItem button  component={Link} to='/subtipo' selected={selectedIndex === 8} onClick={(event:any) => handleListItemClick(event, 8)}>
+                        <ListItem button  component={Link} to='subtipo' selected={selectedIndex === 8} onClick={(event:any) => handleListItemClick(event, 8)}>
                             <ListItemIcon>
                                 <LayersIcon />
                             </ListItemIcon>
                             <ListItemText primary="Subtipo de incidencia" />
                         </ListItem>
-                        <ListItem button  component={Link} to='/tipo-accion' selected={selectedIndex === 9} onClick={(event:any) => handleListItemClick(event, 9)}>
+                        <ListItem button  component={Link} to='tipo-accion' selected={selectedIndex === 9} onClick={(event:any) => handleListItemClick(event, 9)}>
                             <ListItemIcon>
                                 <LayersIcon />
                             </ListItemIcon>
